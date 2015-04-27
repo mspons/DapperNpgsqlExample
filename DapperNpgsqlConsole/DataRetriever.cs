004e77620ff73313f06f5db6d5dda8af943d4fe6
Mark Sponsler <mark.sponsler@gmail.com>
2015-04-27 20:11:16 +0100

    Break out classes into multiple files

---
+++ DapperNpgsqlConsole/DataRetriever.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using Dapper;
+using Npgsql;
+
+namespace DapperNpgsqlConsole
+{
+    public class DataRetriever : IDisposable
+    {
+        private readonly IDbConnection dbConnection;
+
+        public DataRetriever()
+        {
+            dbConnection = new NpgsqlConnection("Server=192.168.2.4;Port=5432;User Id=beer_tracker_dev;Password=btdev;Database=beer_tracker_dev;");
+            dbConnection.Open();
+        }
+
+        public IEnumerable<Beer> GetBeers()
+        {
+            return dbConnection.Query<Beer>("select * from beers");
+        }
+
+        public IEnumerable<Style> GetStyles()
+        {
+            return dbConnection.Query<Style>("select * from styles");
+        }
+
+        public void Dispose()
+        {
+            dbConnection.Close();
+        }
+    }
+}
---
+++ DapperNpgsqlConsole/Style.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperNpgsqlConsole
+{
+    public class Style
+    {
+        public int Style_Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
