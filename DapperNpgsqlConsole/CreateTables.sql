7091559bc8ba1264b351ed2bbb1d3de2557f585c
Mark Sponsler <Mark.Sponsler@sintecmedia.com>
2015-04-27 19:27:25 +0100

    Initial commit

---
+++ DapperNpgsqlConsole/CreateTables.sql
@@ -0,0 +1,13 @@
+﻿/* Create tables */
+CREATE TABLE styles (
+	style_id serial PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	description TEXT NOT NULL
+);
+
+CREATE TABLE beers (
+	beer_id serial PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	style_id integer REFERENCES styles(style_id),
+	abv decimal(4,1) 
+);
